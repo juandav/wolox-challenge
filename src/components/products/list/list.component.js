e08150ee338a664cb9aec00d243fa027c4561157
juandav <e.juandav@gmail.com>
2019-07-22 04:41:14 -0500

    update

--- src/components/products/list/list.component.js
+++ src/components/products/list/list.component.js
@@ -6,7 +6,7 @@ const List = props => {
     <div className="product">
       {(products || []).map(item => (
         <div key={item.id} className="product__card">
-          <img className="product__image" src="https://hniesfp.imgix.net/8/images/detailed/94/01-02010100R3G1_6.jpg?fit=fill&bg=0FFF&w=1500&h=1000&auto=format,compress" alt="product" />
+          <img className="product__image" src={item.image_url} alt="product" />
           <div className="product__content">
             <span className="product__name">{item.name}</span>
             <br />
